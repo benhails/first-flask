b93408cd368af5cba4514ab95a75b2c7da48a183
Ben Hails <benhails@hotmail.com>
2020-01-17 10:03:37 +0800

    included template inheritance

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/<name>') # Revisit decorators if you unclear of this syntax
+@app.route('/<name>', methods=["GET"]) # Revisit decorators if you unclear of this syntax
 def index(name):
     name = name.upper()
     signed_in = False
     return render_template('index.html', name=name, signed_in=signed_in)
 
-@app.route('/another')
+@app.route('/')
 def show():
     return '<h1>Yo</h1>'
 
-@app.route('/user/<username>')
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@app.route('/user/<string:username>') # adding string will ensure the username is a string otherwise no match
 def user_page(username):
     return f'Hi {username[0:3]}'
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
-    app.run()
-    # app.run(debug=True)
+    # app.run()
+    app.run(debug=True)
